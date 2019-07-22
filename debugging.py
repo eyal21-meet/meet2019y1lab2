73ea9ca829ebd225f8a2e35d8c236a708063fa53
eyal21-meet <eyal21@meet.mit.edu>
2019-07-22 09:52:06 -0400

    Stopping here

---
+++ debugging.py
@@ -0,0 +1,5 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: /home/student/eyal21_lab2/meet2019y1lab2/debuging turtle.py ====
--- lab_2.py
+++ lab_2.py
@@ -1,1 +1,17 @@
-type(259-33.0)
+a = 5
+b = 10
+c = a
+a = b
+b = c
+a
+b
+four = "4"
+print(four*3)
+my_name = "student"
+print("Hi, " + my_name)
+age = "15"
+print("I am " + age + " years old")
+score = 4
+count = 5
+total = score * count
+print(total)
